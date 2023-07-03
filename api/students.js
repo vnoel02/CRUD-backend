b2ce3ca14507183c5795f25d24bb3b3eb010010e
victornoel <vnoel890@gmail.com>
2023-07-03 10:42:37 -0400

    Added post function for a new student

--- api/students.js
+++ api/students.js
@@ -13,4 +13,23 @@ router.get("/", async (req, res, next) => {
     }
   });
 
+  // Post - create a new student
+  router.post("/newstudent", async (req, res, next) => {
+    try {
+      const newStudent = await Students.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        address: req.body.address,
+        GPA: req.body.GPA,
+        campusId: req.body.campusId
+      });
+      newStudent
+        ? res.status(200).json(newStudent)
+        : res.status(404).json("No info provided");
+    } catch (error) {
+      next(error);
+    }
+  });
+
   module.exports = router;
