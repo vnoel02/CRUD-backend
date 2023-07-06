4b3155db3d47df873f22457350c14ed31a8b0d8c
victornoel <vnoel890@gmail.com>
2023-07-06 15:48:00 -0400

    Trying vercel deploy

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.use("/api", require("./api")); //Mounting api for routing via express
 // Potential sync, place db.sync({force: true }) to nuke data
 const syncDB = () => db.sync();
 
+app.get("/", (req,res) => {
+  res.status(200).send(
+    <div>
+      <h1> Welcome to Victor's CRUD-backend</h1>
+    </div>
+  )
+})
 
 const runServer = () => {
   app.listen(PORT, () => {
