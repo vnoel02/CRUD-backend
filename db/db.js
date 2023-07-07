5033cd037c8ac769ce910aed3d6777f30382f832
victornoel <vnoel890@gmail.com>
2023-07-07 09:59:15 -0400

    adding pg

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 // connects sequelize with the CRUD_backend database
 const { Sequelize } = require("sequelize");
 const {name} = require("../package.json");
+const {pg} = require("pg")
 
 const db = new Sequelize(`postgres://localhost:5432/${name}`, {
     logging: false,
